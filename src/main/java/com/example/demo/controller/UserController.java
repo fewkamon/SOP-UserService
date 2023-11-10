685476e7adc18a3a9a90cb24c5253f277f205bc9
Few <64070001@kmitl.ac.th>
2023-11-10 22:19:02 +0700

    update

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.repository.UserService;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -14,6 +16,7 @@ import java.util.Optional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@RequestMapping("/api")
 @RestController
 public class UserController {
 
@@ -24,34 +27,33 @@ public class UserController {
     private JwtService jwtService;
 
     @RequestMapping(value ="/login", method = RequestMethod.POST)
-    public Map<String, Object> loginUser(@RequestBody LoginDTO wz) {
+    public ResponseEntity<?> loginUser(@RequestBody LoginDTO wz) {
         System.out.println(wz.getEmail());
         System.out.println(wz.getPassword());
-
         User data = this.userService.CheckLogin(wz.getEmail(), wz.getPassword());
         if (data == null) {
             Map<String, Object> responseData = new HashMap<>();
             responseData.put("status", "error");
             responseData.put("message", "อีเมลหรือรหัสผ่านไม่ถูกต้อง");
-            return responseData;
+            return ResponseEntity.status(404).contentType(MediaType.APPLICATION_JSON).body(responseData);
         } else {
             String token = jwtService.generateToken(data);
             Map<String, Object> responseData = new HashMap<>();
             responseData.put("status", "success");
             responseData.put("message", "อันนี้คือเข้าสู่ระบบแล้ว");
             responseData.put("token", token);
-            return responseData;
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(responseData);
         }
     }
 
     @RequestMapping(value ="/register", method = RequestMethod.POST)
-    public Map<String, Object>  registerUser(@RequestBody String requestBody) {
+    public ResponseEntity<?> registerUser(@RequestBody String requestBody) {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             RegisterDTO registerDTO = objectMapper.readValue(requestBody, RegisterDTO.class);
             String lastName = registerDTO.getInfo().getLast_name();
             String firstName = registerDTO.getInfo().getFirst_name();
-
+            System.out.println(firstName);
             Date currentDate = new Date();
 
             User data = userService.findUserWithEmail(registerDTO.getEmail());
@@ -63,14 +65,15 @@ public class UserController {
 
                 Map<String, Object> responseData = new HashMap<>();
                 responseData.put("status", "success");
-                responseData.put("message", "อันนี้คือสมัครแล้ว");
+                responseData.put("message", "สมัครสำเร็จ");
                 responseData.put("token", token);
-                return responseData;
+                return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(responseData);
             } else {
                 Map<String, Object> responseData = new HashMap<>();
                 responseData.put("status", "error");
-                responseData.put("message", "อันนี้คือเมลมีคนสมัครไปแล้ว");
-                return responseData;
+                responseData.put("message", "มีคนสมัครอีเมลนี้ไปแล้ว");
+                return ResponseEntity.status(409).contentType(MediaType.APPLICATION_JSON).body(responseData);
+
             }
 
         } catch (Exception e) {
@@ -78,7 +81,8 @@ public class UserController {
             Map<String, Object> responseData = new HashMap<>();
             responseData.put("status", "error");
             responseData.put("message", "เกิดข้อผิดพลาดจากระบบ");
-            return responseData;
+            return ResponseEntity.status(404).contentType(MediaType.APPLICATION_JSON).body(responseData);
+
         }
     }
 
@@ -88,36 +92,45 @@ public class UserController {
             String authorizationHeader = request.getHeader("Authorization");
             String[] token = authorizationHeader.split(" ");
             Claims claims = jwtService.parseToken(token[1]);
-
-
             Optional<User> user = this.userService.Me(claims.getSubject().toString());
-
             User userupdate = user.get();
-
             userupdate.setInfo(wz.getInfo());
-
             this.userService.updateUser(userupdate);
             return null;
-
-
         } catch (Exception e) {
             return null;
         }
     }
 
     @RequestMapping(value ="/me", method = RequestMethod.GET)
-    public Optional<User> getUser(HttpServletRequest request) {
+    public ResponseEntity<?> getUser(HttpServletRequest request) {
         try {
             String authorizationHeader = request.getHeader("Authorization");
-            String[] token = authorizationHeader.split(" ");
-            Claims claims = jwtService.parseToken(token[1]);
-            return this.userService.Me(claims.getSubject().toString());
+            if (authorizationHeader != null) {
+                String[] token = authorizationHeader.split(" ");
+                if (token.length > 1) {
+                    Claims claims = jwtService.parseToken(token[1]);
+
+                    Optional<User> userOptional = userService.Me(claims.getSubject().toString());
+                    if (userOptional.isPresent()) {
+                        User user = userOptional.get();
+
+                        ObjectMapper objectMapper = new ObjectMapper();
+                        String userJson = objectMapper.writeValueAsString(user);
+
+                        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(userJson);
+                    } else {
+                        return ResponseEntity.status(404).body("User not found");
+                    }
+                }
+            }
+            return ResponseEntity.status(401).body("Invalid token");
         } catch (Exception e) {
-            return null;
+            return ResponseEntity.status(401).body("Invalid token");
         }
     }
 
-    @RequestMapping(value ="/me/{id}", method = RequestMethod.GET)
+    @RequestMapping(value ="/user/{id}", method = RequestMethod.GET)
     public Optional<User> getUserWithId(@PathVariable("id") String id, HttpServletRequest request) {
         try {
             System.out.println(id);
@@ -128,25 +141,32 @@ public class UserController {
     }
 
     @RequestMapping(value ="/me", method = RequestMethod.DELETE)
-    public Map<String, Object> deleteUser(HttpServletRequest request) {
+    public ResponseEntity<?> deleteUser(HttpServletRequest request) {
         try {
             String authorizationHeader = request.getHeader("Authorization");
-            String[] token = authorizationHeader.split(" ");
-            Claims claims = jwtService.parseToken(token[1]);
-
-
-            this.userService.deleteUser(claims.getSubject().toString());
-
-            Map<String, Object> responseData = new HashMap<>();
-            responseData.put("status", "success");
-            responseData.put("message", "อันนี้คือลบเรียบร้อยแล้ว");
-            return responseData;
+            if (authorizationHeader != null) {
+                String[] token = authorizationHeader.split(" ");
+                if (token.length > 1) {
+                    Claims claims = jwtService.parseToken(token[1]);
+                    this.userService.deleteUser(claims.getSubject().toString());
+                    Map<String, Object> responseData = new HashMap<>();
+                    responseData.put("status", "success");
+                    responseData.put("message", "ทำการลบสมาชิกเรียบร้อย");
+                    return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(responseData);
+                }
+            }
+            return ResponseEntity.status(401).body("Invalid token");
         } catch (Exception e) {
+            e.printStackTrace();
             Map<String, Object> responseData = new HashMap<>();
             responseData.put("status", "error");
             responseData.put("message", "ไม่สามารถทำการลบได้");
-            return responseData;
+            return ResponseEntity.status(404).contentType(MediaType.APPLICATION_JSON).body(responseData);
         }
+
+
+
+
     }
 
 }
